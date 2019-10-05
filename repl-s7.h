4cadee532232f7fbe80df3fc5e59d745c87539ae
Bernard Tatin <bernard.tatin@outlook.fr>
2019-10-05 23:54:10 +0200

    Update repl-s7.h and repl.c: we can load several files

---
+++ repl-s7.h
@@ -0,0 +1,15 @@
+/*
+ * repl-s7.h
+ */
+
+#if !defined(__repl_s7_h__)
+#define __repl_s7_h__
+
+#if !defined(SUCCESS)
+#define SUCCESS 0
+#endif
+#if !defined(FAILURE)
+#define FAILURE 1
+#endif
+
+#endif // __repl_s7_h__
--- repl.c
+++ repl.c
@@ -2,20 +2,26 @@
 #include <stdlib.h>
 
 #include "s7.h"
+#include "repl-s7.h"
 
 int main(int argc, char **argv) {
+    int ret_value = SUCCESS;
     s7_scheme *sc;
     sc = s7_init();
 
-    if (argc == 2) {
-        fprintf(stderr, "load %s\n", argv[1]);
-        if (!s7_load(sc, argv[1]))
-            fprintf(stderr, "can't find %s\n", argv[1]);  /* it could also be a directory */
+    if (argc > 1) {
+        for (int i=1; i<argc && ret_value==SUCCESS; i++) {
+            fprintf(stderr, "load %s\n", argv[i]);
+            if (!s7_load(sc, argv[i])) {
+                fprintf(stderr, "can't find %s\n", argv[i]);  /* it could also be a directory */
+                ret_value = FAILURE;
+            }
+        }
     } else {
         s7_load(sc, "repl.scm");
         s7_eval_c_string(sc, "((*repl* 'run))");
     }
-    return(0);
+    return ret_value;
 }
 
 /* gcc -o repl repl.c s7.o -Wl,-export-dynamic -lm -I. -ldl
