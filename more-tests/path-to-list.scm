694ba52d21bddae07e5e75b9d5be3df2fd90cb98
Bernard Tatin <bernard.tatin@outlook.fr>
2019-10-07 23:26:25 +0200

    path-to-list.scm: hash table added

--- more-tests/path-to-list.scm
+++ more-tests/path-to-list.scm
@@ -8,12 +8,41 @@
         (list element)
         (cons element the-list)))
 
-    (define (mk-path-list acc new-path)
-      (let ((position (char-position #\: new-path)))
-        (if (not position)
-            (reverse (safe-rev-append acc new-path))
-            (mk-path-list (safe-rev-append acc (substring new-path 0 position))
-                          (substring new-path (+ 1 position))))))
-    (mk-path-list '() path))
+  (define (mk-path-list acc new-path)
+    (let ((position (char-position #\: new-path)))
+      (if (not position)
+          (reverse (safe-rev-append acc new-path))
+          (mk-path-list (safe-rev-append acc (substring new-path 0 position))
+                        (substring new-path (+ 1 position))))))
+  (mk-path-list '() path))
 
-(define *path* (path-to-list (getenv "PATH")))
+(define file-exec? 
+  (let () 
+    (c-define '((int X_OK) (int access (char* int))) 
+              "" 
+              "unistd.h") 
+    (lambda (arg) (= (access arg X_OK) 0))))
+
+(define (fill-path-hash path)
+  (let ((the-hash (make-hash-table 8192)))
+    (define (explore-path path)
+      (define (add-file file)
+        (let ((full-path (string-append path "/" file)))
+          (when (and (file-exec? full-path)
+                     (not (the-hash file)))
+            (set! (the-hash file) full-path))))
+      (let ((all-files (directory->list path)))
+        (for-each add-file all-files)))
+    (begin
+      (for-each explore-path path)
+      the-hash)))
+
+(define *path* '())
+(define *path-hash* '())
+
+
+(define (init-path new-path)
+  (set! *path* (path-to-list new-path))
+  (set! *path-hash* (fill-path-hash *path*)))
+
+(init-path (getenv "PATH"))
--- more-tests/test-path.scm
+++ more-tests/test-path.scm
@@ -4,3 +4,8 @@
 
 (format #t "PATH: ~A~%~%~%" (getenv "PATH"))
 (for-each (lambda(element) (format #t "-> ~A~%" element)) *path*)
+(format #t "-------------------------------~%")
+(for-each (lambda(element)
+            (format #t "~A -> ~A~%" (car element) (cdr element)))
+          *path-hash*)
+
