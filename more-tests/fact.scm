be6ebb275bac2e5407f45325bf05498bac0c7858
Bernard Tatin <bernard.tatin@outlook.fr>
2019-10-14 00:41:47 +0200

    fact.scm: following lint recommendations

--- more-tests/fact.scm
+++ more-tests/fact.scm
@@ -4,10 +4,20 @@
 
 (provide 'fact.scm)
 
-(define (fact N)
+(define (fact0 N)
   (define (inner-fact acc k)
     (if (< k 2)
         acc
         (inner-fact (* k acc) (- k 1))))
 
   (inner-fact 1 N))
+
+(define fact 
+  (let ()
+    (define (inner-fact acc k) 
+      (if (< k 2)
+          acc
+          (inner-fact (* k acc) (- k 1))))
+    (lambda (N)
+      (inner-fact 1 N))))
+
