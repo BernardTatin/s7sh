728cba3023afacc34f95a18cafb75f6079e6101f
Bernard Tatin <bernard.tatin@outlook.fr>
2019-10-15 13:35:13 +0200

    full-provide: cleaner code

--- libs/basic-lib.scm
+++ libs/basic-lib.scm
@@ -21,47 +21,41 @@
 
 ;;
 ;; full-provide sym (a symbol)
-;; add sym tu *features,
+;; add sym to *features,
 ;; add the directory of the current file to *load-path*
 ;; TODO: errors management!
-;; TODO: must be a macro
-;; NOTE: *ql_sym* is a bad hack in case of mult-threading
-;; NOTE: or in a simpler way: *ql_sym* is a bad hack 
-
-(define *gl_sym* #f)
 (define-macro (full-provide sym)
               `(let ((provide-sym (lambda()
                                     (when (not (in-features? ,sym))
-                                      (set! *gl_sym* ,sym)
                                       (with-let (rootlet)
-                                                (provide *gl_sym*))
-                                      (set! *gl_sym* #f)))))
-                 (let ((symname (symbol->string ,sym)))
-                   (let ((directory 
-                           (let ((current-file (port-filename)))
-                             (begin
-                               ;; TODO: verify problems with length
-                               (format #t "<current-file: ~A~%" current-file)
-                               (and (memv (current-file 0) '(#\/ #\~ #\.))
-                                    (substring 
-                                      current-file 
-                                      0 
-                                      (- (length current-file) 
-                                         (+ 1 (length symname)))))))))
+                                                (provide ,sym))))))
+                 (let ((directory 
+                         (let ((current-file (port-filename)))
+                           (begin
+                             ;; TODO: verify problems with length
+                             (and (memv (current-file 0) '(#\/ #\~ #\.))
+                                  (substring 
+                                    current-file 
+                                    0 
+                                    (- (length current-file) 
+                                       (+ 1 (length (symbol->string ,sym))))))))))
+                   (when (and directory (not (in-load-path? directory)))
                      (provide-sym)
-                     (when (and directory (not (in-load-path? directory)))
-                       (set! *load-path* (cons directory *load-path*)))))))
+                     (set! *load-path* (cons directory *load-path*))))))
 
 ;; using full-provide
 (full-provide 'basic-lib.scm)
 
 ;; debugging full-provide
-(let ((show-list (lambda (list-name L)
-                   (format #t "-------------------------------~%")
-                   (format #t "~A~%" list-name)
-                   (for-each (lambda(e)
-                               (format #t "+ ~A~%" e))
-                             L))))
+(when (not *quiet*)
+  (let ((show-list (lambda (list-name L)
+                     (format #t "-------------------------------~%")
+                     (format #t "~A~%" list-name)
+                     (pretty-print L)
+                     (format #t "~%~%"))))
 
-  (show-list "*features*" *features*)
-  (show-list "*load-path*:" *load-path*))
+    (show-list "*features*" *features*)
+    (show-list "*load-path*:" *load-path*)))
+(format #t "~%")
+(pretty-print (macroexpand (full-provide 'basic-lib.scm)))
+(format #t "~%~%")
