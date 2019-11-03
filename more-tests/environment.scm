6852e17fd2a3b15c49fcc5e549aa243d23775236
Bernard (SunOs) <bernard.tatin@outlook.fr>
2019-11-03 23:13:03 +0100

    environment.scm: a good example (with weakness)

--- more-tests/environment.scm
+++ more-tests/environment.scm
@@ -1,18 +1,56 @@
 ;;
 ;; environment.scm
 ;;
+;; Using with-let and curlet
 
 (xt-provide 'environment.scm)
+(load "write.scm")
 
-(when (not (defined? '*t-env*))
-  (define *t-env*
+(when (not (defined? '*t-env-0*))
+  (define *t-env-0*
     (with-let (unlet)
-              (let ((show-me (lambda(msg)
-                (format #t "This is env (~A)~%" msg))))
-              (show-me "*t-env* is loading... loading...")
-    (curlet)))))
+              (begin
+                (let ((show-me (lambda(msg)
+                                 (format #t "This is env (~A)~%" msg))))
+                  (show-me "*t-env-0* is loading... loading...")
+                  (curlet))))))
+
+*t-env-0*
+((*t-env-0* 'show-me) "oh... we are testing *t-env-0* !!!")
+
+(define-macro (define-new-env env-name . body)
+              `(when (not (defined? ',env-name))
+                 (define ,env-name
+                   (with-let (unlet)
+                             (begin
+                               ,@body
+                               (curlet))))))
+
+(define-macro (pretty-expand . body)
+              `(begin
+                 (format #t "~%~%")
+                 (pretty-print (macroexpand ,@body))
+                 (format #t "~%~%")))
+
+(pretty-expand
+  (define-new-env *t-env*
+                  (define (show-me msg)
+                    (format #t "This is env (~A)~%" msg))
+                  (define (re-show-me msg)
+                    (format #t "This is env, one more time (~A)~%" msg))
+                  (show-me "*t-env* is loading... loading...")
+                  (re-show-me "*t-env* is loaded ?")))
+;; (format #t "~%~%")
+
+(define-new-env *t-env*
+                (define (show-me msg)
+                  (format #t "This is env (~A)~%" msg))
+                (define (re-show-me msg)
+                  (format #t "This is env, one more time (~A)~%" msg))
+                (show-me "*t-env* is loading... loading...")
+                (re-show-me "*t-env* is loaded ?"))
 
 *t-env*
-
 ;;; simple test
 ((*t-env* 'show-me) "oh... we are testing *t-env* !!!")
+((*t-env* 're-show-me) "oh... we are testing *t-env* !!!")
