ad8a18f69f7395fb1c873e1bc0d0e44f50569bb7
Bernard (SunOs) <bernard.tatin@outlook.fr>
2019-11-04 11:09:42 +0100

    more-tests/play-with-methods.scm: creation

--- more-tests/fact.scm
+++ more-tests/fact.scm
@@ -3,7 +3,6 @@
 ;;
 
 (xt-provide 'fact.scm)
-(load "stuff.scm")
 
 (define (fact0 N)
   (define (inner-fact acc k)
@@ -21,37 +20,3 @@
           (inner-fact (* k acc) (- k 1))))
     (lambda (N)
       (inner-fact 1 N))))
-
-(define-class Int ()
-              '((value 0)))
-
-(define (new-Int k)
-  (if (not (integer? k))
-    #f
-    (let ((i (make-Int)))
-      (set! (i 'value) k)
-      i)))
-
-(define-generic factg)
-(define-method (factg (N Int))
-               (define (inner-fact acc k)
-                 (if (< k 2)
-                   acc
-                   (inner-fact (* k acc) (- k 1))))
-
-               (inner-fact 1 (N 'value)))
-
-(define-method (decN (N Int))
-               (set! (N 'value) (- (N 'value) 1)))
-
-(define-method (incN (N Int))
-               (set! (N 'value) (+ (N 'value) 1)))
-
-
-(define-method (display-fact-N (N Int))
-               (let ((vN (N 'value)))
-                 (format #t "~A! = ~A~%"
-                         vN
-                         (factg N))))
-(define N (new-Int 5))
-(display-fact-N N)
---
+++ more-tests/play-with-methods.scm
@@ -0,0 +1,73 @@
+;;
+;; play-with-methods
+;;
+
+(load "stuff.scm")
+(when (not (defined? 'pretty-print))
+  (load "write.scm"))
+
+(define-macro (for-int idx from to initialize . body)
+              `(let ()
+                 (begin
+                   ,initialize)
+                 (define (ifrom ,idx)
+                   (when (< ,idx ,to)
+                     (begin
+                       ,@body)
+                       (ifrom (+ ,idx 1))))
+                 (ifrom ,from)))
+(pretty-expand
+  (for-int k 0 15
+           (begin
+             (format #t "Fact from 0 to 14")
+             (define nK (new-Int 0)))
+           (format #t "~3D -- " k)
+           (display-fact-N nK)
+           (incN nK)))
+
+(define-class Int ()
+              '((value 0)))
+
+(define (new-Int k)
+  (if (not (integer? k))
+    #f
+    (let ((i (make-Int)))
+      (set! (i 'value) k)
+      i)))
+
+(define-generic factg)
+(define-method (factg (N Int))
+               (define (inner-fact acc k)
+                 (if (< k 2)
+                   acc
+                   (inner-fact (* k acc) (- k 1))))
+
+               (inner-fact 1 (N 'value)))
+
+(define-method (decN (N Int))
+               (set! (N 'value) (- (N 'value) 1)))
+
+(define-method (incN (N Int))
+               (set! (N 'value) (+ (N 'value) 1)))
+
+
+(define-method (display-fact-N (N Int))
+               (let ((vN (N 'value)))
+                 (format #t "~3D! = ~A~%"
+                         vN
+                         (factg N))))
+(for-int k 0 15
+           (begin
+             (format #t "Fact from 0 to 14~%")
+             (define nK (new-Int 0)))
+           (format #t "~3D -- " k)
+           (display-fact-N nK)
+           (incN nK))
+
+(for-int k 0 15
+           (begin
+             (format #t "Fact from 14 to 0~%")
+             (define nK (new-Int 14)))
+           (format #t "~3D -- " k)
+           (display-fact-N nK)
+           (decN nK))
